7b2e900ec6c80e011c0367a85013224b4e89dedc
Houngla Linky <baisss2000@sina.com>
2023-06-06 20:53:24 +0800

    优化

--- src/main/java/com/example/server/controller/LoginController.java
+++ src/main/java/com/example/server/controller/LoginController.java
@@ -7,8 +7,6 @@ import com.example.server.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 public class LoginController {
     @Autowired
